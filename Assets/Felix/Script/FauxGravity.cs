6eefc87c26ebbd4db4d37b3cf0488fc57279c733
Felix Szczesny <felix@maschinen-mensch.com>
2021-03-07 15:59:02 +0100

    gravity system done

---
+++ Assets/Felix/Script/FauxGravity.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravity : MonoBehaviour
+{
+    public float gravity = -10;
+    public float roationSmoothness;
+    public void Attract(Transform body)
+    {
+        Vector3 gravityDown = (body.position - new Vector3(body.position.x, 0, 0)).normalized;
+        Vector3 gravityUp = gravityDown * -1;
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(body.up, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, roationSmoothness * Time.deltaTime);
+    }
+}
