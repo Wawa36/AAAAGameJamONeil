9f2b3d2ea611cc79858c5ef29df145444e208b23
Felix Szczesny <felix@maschinen-mensch.com>
2021-03-07 17:38:04 +0100

    merged movement and grapling

--- Assets/Lila/Grappling.cs
+++ Assets/Lila/Grappling.cs
@@ -15,6 +15,16 @@ public class Grappling : MonoBehaviour
     private Vector3 hookshotPosition;
     private Ray ray;
 
+    //felix
+    public float moveSpeed = 10;
+    public Vector3 EulerAngleVelocity = new Vector3(0, 5, 0);
+    private Vector3 moveDir;
+    private Rigidbody rigibody;
+
+    private void Start()
+    {
+        rigibody = this.GetComponent<Rigidbody>();
+    }
 
     private enum State
     {
@@ -36,13 +46,52 @@ public class Grappling : MonoBehaviour
             default:
             case State.Normal:
                 HandleHookshotStart();
+                HandleCharacterLook();
                 Debug.Log("normal");
                 break;
             case State.HookshotFlyingPlayer:
                 Debug.Log("flying");
+                HandleCharacterLook();
+                break;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        switch (state)
+        {
+            default:
+            case State.Normal:
+                Movement();
+                break;
+            case State.HookshotFlyingPlayer:
                 HandleHookshotMovement();
                 break;
         }
+
+    }
+
+    private void HandleCharacterLook()
+    {
+        float lookX = Input.GetAxisRaw("Mouse X");
+        float lookY = Input.GetAxisRaw("Mouse Y");
+
+        transform.Rotate(new Vector3(0f, lookX * mouseSensitivity, 0f), Space.Self);
+        cameraVerticalAngle -= lookY * mouseSensitivity;
+        cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -89f, 89f);
+        playerCamera.transform.localEulerAngles = new Vector3(cameraVerticalAngle, 0, 0);
+    }
+
+    private void Movement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        moveDir = this.transform.forward * vertical;
+
+        rigibody.MovePosition(rigibody.position + (moveDir * moveSpeed * Time.deltaTime));
+        float horizontal = Input.GetAxis("Horizontal");
+        Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * horizontal * Time.fixedDeltaTime);
+        rigibody.MoveRotation(rigibody.rotation * deltaRotation);
     }
 
     private void HandleHookshotStart()
@@ -69,12 +118,12 @@ public class Grappling : MonoBehaviour
     {
         Vector3 hookshotDir = (hookshotPosition - transform.position).normalized;
 
-        float hookshotSpeedMin = 10f;
-        float hookshotSpeedMax = 20f;
+        float hookshotSpeedMin = 0.1f;
+        float hookshotSpeedMax = 1f;
         float hookshotSpeed = Mathf.Clamp(Vector3.Distance(transform.position, hookshotPosition), hookshotSpeedMin, hookshotSpeedMax);
-        float hookshotSpeedMultiplier = 5f;
+        float hookshotSpeedMultiplier = 1f;
 
-        GetComponent<Rigidbody>().AddForce(hookshotDir * hookshotSpeed * hookshotSpeedMultiplier);
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + hookshotDir * hookshotSpeed * hookshotSpeedMultiplier);
         //characterController.Move(hookshotDir * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime);
         //transform.position = Vector3.MoveTowards(transform.position, hookshotPosition, .5f);
 
