ab17013aaa4e8533e4141ee3e1f7c29ae19aa47c
Felix Szczesny <felix@maschinen-mensch.com>
2021-03-07 14:03:49 +0100

    gravity wip

--- Assets/Felix/Script/GravityScript.cs
+++ Assets/Felix/Script/GravityScript.cs
@@ -7,16 +7,12 @@ public class GravityScript : MonoBehaviour
     public float gravityConst;
     public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 gravityDirection = player.transform.position - new Vector3(player.transform.position.x, 0, 0);
         Physics.gravity = gravityDirection.normalized * gravityConst;
+
+        //player.transform.up = -gravityDirection.normalized;
+       
     }
 }
